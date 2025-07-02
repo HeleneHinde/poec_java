537b8e7905d5c7199c7818c47e0ac18d1d243e5c
HeleneHinde <helene.mazgaldi@gmail.com>
2025-07-02 11:09:03 +0200

    init

--- src/main/java/formationjava/TestFormation.java
+++ src/main/java/formationjava/TestFormation.java
@@ -2,8 +2,9 @@ package formationjava;
 
 public class TestFormation {
     public static void main(String[] args) {
-        System.out.println("Hello, Formation Java!");
-
+        int entier = 100;
+        System.out.println("Valeur de entier : " + entier);
+        
         
     }
 }
